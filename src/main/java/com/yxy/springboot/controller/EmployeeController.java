c5e8a133b3fadac80e21b4afef5c029c729d6464
yuxiangying <1044943425@com>
2019-07-03 17:46:06 +0800

    20190703

--- src/main/java/com/yxy/springboot/controller/EmployeeController.java
+++ src/main/java/com/yxy/springboot/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.yxy.springboot.controller;
 
 import com.yxy.springboot.dao.EmployeeDao;
+import com.yxy.springboot.entities.Department;
 import com.yxy.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 import java.util.List;
@@ -32,4 +36,43 @@ public class EmployeeController {
         model.addAttribute("emps",employees);
         return "emp/list";
     }
+
+    //来到员工添加页面
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //来到添加页面，查出所有的部门，在页面上显示
+        List<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps",employees);
+        return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定；要求请求参数的名字和javaBean入参的对象里面属性一一对应
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+        System.out.println("保存的员工信息："+employee);
+        //employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        //Employee employee = employeeDao.get(id);
+        //model.addAttribute("emp",employee);
+        //页面所有部门列表
+       // List<Department> departments = departmentDao.getAll();
+        //model.addAttribute("depts",departments);
+        //回到修改页面（add是一个修改添加二合一的页面）
+        return "emp/add";
+    }
+
+    //员工修改
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee){
+        System.out.println("保存的员工信息："+employee);
+        //employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
 }
